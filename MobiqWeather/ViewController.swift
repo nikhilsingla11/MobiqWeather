a08de2c7bf2ccf8a018545c1c6d8df6998b195b4
Nikhil Aggarwal <Nikhilsingla11@gmail.com>
2020-12-25 15:59:07 +0530

    Initial Commit

---
+++ MobiqWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobiqWeather
+//
+//  Created by Nikhil Aggarwal on 25/12/20.
+//  Copyright © 2020 Demo Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
